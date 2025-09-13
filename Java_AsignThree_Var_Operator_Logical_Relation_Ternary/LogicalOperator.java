3157dc5e7cd85f77022198d316cd84c71f233f1c
marufsagor <marufsagor10@gmail.com>
2025-09-13 11:15:51 +0600

    NewBranchAsiignment3

---
+++ Java_AsignThree_Var_Operator_Logical_Relation_Ternary/LogicalOperator.java
@@ -0,0 +1,14 @@
+package Java_AsignThree_Var_Operator_Logical_Relation_Ternary;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+
+        int a=80;
+        int b=85;
+
+        System.out.println("a < b && a!=b && a!=b:" + (a < b && a!=b ) );
+        System.out.println("a < b || a!=b && a!=b:" + (a > b || a!=b ) );
+
+    }
+
+}
